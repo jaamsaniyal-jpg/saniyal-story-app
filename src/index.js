09dc757528a740cfd002d396d71c03d7853ba557
jaamsaniyal-jpg <jaamsaniyal@gmail.com>
2026-02-24 07:16:10 +0500

    Create index.js to render the App component

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<React.StrictMode><App /></React.StrictMode>);
+```
+
+**4)** **"Commit changes"** ✅
+
+---
+
+**Phir:**
+
+**5)** **"Add file"** → **"Create new file"**
+
+**6)** File naam:
+```
+src/App.js
